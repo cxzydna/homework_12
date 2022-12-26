f641c5576234b4b69b5aec6f0f8241e65cfd52d3
Andy <blackwhite901@mail.ru>
2022-12-26 20:47:44 +0300

    create a repository

---
+++ README.md
@@ -0,0 +1,13 @@
+# Homework 12
+# Created by andyzxc
+
+***
+This web application allows you to search, view and download posts
+***
+The following technologies were used in this work :
+*  Flask
+* Python
+* HTML
+* CSS
+***
+Thanks for your attention
---
+++ loader/views.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, render_template, request
+import logging
+
+from loader.utils import save_picture
+from main.utils import PostHandler
+
+loader_blueprint = Blueprint('loader_blueprint', __name__,
+                             template_folder='templates')
+logging.basicConfig(filename='basic.log', level=logging.INFO)
+
+
+@loader_blueprint.route('/post')
+def create_new_post_page():
+    return render_template('post_form.html')
+
+
+@loader_blueprint.route('/post', methods=['POST'])
+def create_new_post():
+    picture = request.files.get('picture')
+    content = request.form.get('content')
+
+    if not picture or not content:
+        return '<h1>Не все поля заполнены</h1>'
+
+    picture_path = save_picture(picture)
+    if not picture_path:
+        logging.info('Загружено не изображение')
+        return '<h1>Загружено не изображение</h1>'
+
+    post_handler = PostHandler('posts.json')
+    new_post = {'pic': picture_path, 'content': content}
+    post_handler.add_post(new_post)
+
+    return render_template('post_uploaded.html', picture_path=picture_path,
+                           content=content)
